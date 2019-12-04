7cc0c1979ed56f9931bdfa08db209d1979ab6a58
Joseph Orzechowski <jorzech88@hotmail.com>
2019-12-04 07:44:13 -0600

    Initial commit

---
+++ js/countdown.js
@@ -0,0 +1,59 @@
+
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 26, 2020 15:00 GMT -5").getTime();
+
+( function calcCountDown() {
+
+
+  var now = new Date().getTime();
+
+  var toEvent = countDownDate - now;
+
+ 
+  if (toEvent > 0) {
+
+    // Time calculations for days, hours, minutes and seconds
+
+    var days = Math.floor(toEvent / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((toEvent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((toEvent % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((toEvent % (1000 * 60)) / 1000);
+
+    if (days == 1) {
+    var txtdays = " day ";
+    } else {
+    var txtdays = " days ";
+    }
+ 
+    if (hours == 1) {
+    var txthours = " hour ";
+    } else {
+    var txthours = " hours ";
+    }
+    if (minutes == 1) {
+    var txtminutes = " minute ";
+    } else {;
+    var txtminutes = " minutes "
+    }
+    if (seconds == 1) {
+    var txtseconds = " second ";
+    } else {
+    var txtseconds = " seconds ";
+    }
+  
+    
+    // Output the result in an element with id="countdownjs"
+   
+    
+   document.getElementById("countdownjs").innerHTML = "Count Down to Pigger 2020! <br>"
+     + days + txtdays + hours + txthours + minutes + txtminutes ;
+
+    // set timeout to 15 seconds - it's just minutes now but make sure to change this if it goes back to seconds
+    setTimeout(calcCountDown, 15000);
+
+  }
+  
+  }) ();
+
+
+
